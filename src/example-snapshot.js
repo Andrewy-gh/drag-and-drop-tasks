34bd6fec783b2628d3f054828f963420bb684bbf
Andy-git985 <andy.yu617@gmail.com>
2022-12-23 23:45:54 -0500

    background color of dragging and dropping

---
+++ src/example-snapshot.js
@@ -0,0 +1,11 @@
+// Draggable
+const draggableSnapshot = {
+  isDragging: true,
+  draggingOver: 'column-1', // null if not over a droppable
+};
+
+// Droppable
+const droppableSnapshot = {
+  isDraggingOver: true, // true when a droppable is being dragged over
+  draggingOverWith: 'task-1', // null when droppable is not being dragged over
+};
